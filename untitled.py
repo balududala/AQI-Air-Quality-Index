3a3da5c57bc49421c757a48b7372af8fd7909010
balududala <115579188+balududala@users.noreply.github.com>
2025-10-15 09:46:33 +0530

    Add files via upload

---
+++ untitled.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+# Load your dataset
+data = pd.read_csv('city_day.csv')  # replace with your file name
+data.head()
